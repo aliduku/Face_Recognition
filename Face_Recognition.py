2efd48c5debaa266e076ebdf5bb9182e7b6a9137
Ali Essam <aliduku@gmail.com>
2023-08-23 12:18:11 +0300

    second

--- Face_Recognition.py
+++ Face_Recognition.py
@@ -1,90 +1,105 @@
 import face_recognition
-import av
 import cv2
 import numpy as np
-import streamlit as st
-from streamlit_webrtc import webrtc_streamer, VideoProcessorBase, RTCConfiguration
 
 # Load a sample picture and learn how to recognize it.
+obama_image = face_recognition.load_image_file("barackObama.jpg")
+obama_face_encoding = face_recognition.face_encodings(obama_image)[0]
+
+# Load a second sample picture and learn how to recognize it.
+elon_image = face_recognition.load_image_file("elonMusk.jpg")
+elon_face_encoding = face_recognition.face_encodings(elon_image)[0]
+
+woods_image = face_recognition.load_image_file("tigerWoods.jpeg")
+woods_face_encoding = face_recognition.face_encodings(woods_image)[0]
+
 ali_image = face_recognition.load_image_file("aliEssam.jpg")
 ali_face_encoding = face_recognition.face_encodings(ali_image)[0]
 
 # Create arrays of known face encodings and their names
 known_face_encodings = [
+    obama_face_encoding,
+    elon_face_encoding,
+    woods_face_encoding,
     ali_face_encoding
 ]
 
 known_face_names = [
+    "Barack Obama",
+    "Elon Musk",
+    "Tiger Woods",
     "Ali Essam"
 ]
 
-# Initialize variables
+# Initialize some variables
 face_locations = []
 face_encodings = []
 face_names = []
 process_this_frame = True
 
-class FaceRecognitionProcessor(VideoProcessorBase):
-    def __init__(self):
-        self.processed_frame = None
+capture = cv2.VideoCapture(0)
 
-    def recv(self, frame):
-        # Convert to ndarray
-        frame_data = frame.to_ndarray(format="bgr24")
+while True:
+    
+    # Grab a single frame of video
+    ret, frame = capture.read()
 
-        # Resize frame for faster face recognition processing
-        small_frame = cv2.resize(frame_data, (0, 0), fx=0.25, fy=0.25)
-        rgb_small_frame = np.ascontiguousarray(small_frame[:, :, ::-1])
+    # Resize frame of video to 1/4 size for faster face recognition processing
+    small_frame = cv2.resize(frame, (0, 0), fx=0.25, fy=0.25)
 
-        if process_this_frame:
-            # Find all the faces and face encodings in the current frame of video
-            face_locations = face_recognition.face_locations(rgb_small_frame)
-            face_encodings = face_recognition.face_encodings(rgb_small_frame, face_locations)
+    # Convert the image from BGR color (which OpenCV uses) to RGB color (which face_recognition uses)
+    rgb_small_frame = np.ascontiguousarray(small_frame[:, :, ::-1])
 
-            face_names = []
-            for face_encoding in face_encodings:
-                # See if the face is a match for the known face(s)
-                matches = face_recognition.compare_faces(known_face_encodings, face_encoding)
-                name = "Unknown"
+    # Only process every other frame of video to save time
+    if process_this_frame:
+        # Find all the faces and face encodings in the current frame of video
+        face_locations = face_recognition.face_locations(rgb_small_frame)
+        face_encodings = face_recognition.face_encodings(rgb_small_frame, face_locations)
 
-                # Or instead, use the known face with the smallest distance to the new face
-                face_distances = face_recognition.face_distance(known_face_encodings, face_encoding)
-                best_match_index = np.argmin(face_distances)
-                if matches[best_match_index]:
-                    name = known_face_names[best_match_index]
+        face_names = []
+        for face_encoding in face_encodings:
+            # See if the face is a match for the known face(s)
+            matches = face_recognition.compare_faces(known_face_encodings, face_encoding)
+            name = "Unknown"
 
-                face_names.append(name)
+            # # If a match was found in known_face_encodings, just use the first one.
+            # if True in matches:
+            #     first_match_index = matches.index(True)
+            #     name = known_face_names[first_match_index]
 
-        # Display the results on the frame
-        for (top, right, bottom, left), name in zip(face_locations, face_names):
-            # Scale back up face locations since the frame we detected in was scaled to 1/4 size
-            top *= 4
-            right *= 4
-            bottom *= 4
-            left *= 4
+            # Or instead, use the known face with the smallest distance to the new face
+            face_distances = face_recognition.face_distance(known_face_encodings, face_encoding)
+            best_match_index = np.argmin(face_distances)
+            if matches[best_match_index]:
+                name = known_face_names[best_match_index]
 
-            # Draw a box around the face
-            cv2.rectangle(frame_data, (left, top), (right, bottom), (0, 255, 0), 2)
+            face_names.append(name)
 
-            # Draw a label with a name below the face
-            cv2.rectangle(frame_data, (left, bottom - 35), (right, bottom), (0, 255, 0), cv2.FILLED)
-            font = cv2.FONT_HERSHEY_DUPLEX
-            cv2.putText(frame_data, name, (left + 4, bottom - 4), font, 1, (0, 0, 255), 2)
+    process_this_frame = not process_this_frame
 
-        # Convert the modified frame to av.VideoFrame
-        self.processed_frame = av.VideoFrame.from_ndarray(frame_data, format='bgr24')
-        return self.processed_frame
+    # Display the results
+    for (top, right, bottom, left), name in zip(face_locations, face_names):
+        # Scale back up face locations since the frame we detected in was scaled to 1/4 size
+        top *= 4
+        right *= 4
+        bottom *= 4
+        left *= 4
 
-def app():
-    st.title("Face Recognition using Face_Recognition Library")
-    st.write("Press the button below to start recognizing faces from your webcam")
+        # Draw a box around the face
+        cv2.rectangle(frame, (left, top), (right, bottom), (0, 255, 0), 2)
 
-    # Initialize the webrtc_streamer with the FaceRecognitionProcessor class
-    webrtc_ctx = webrtc_streamer(
-        key="face-recognition",
-        video_processor_factory=lambda: FaceRecognitionProcessor(),
-        rtc_configuration=RTCConfiguration({"iceServers": [{"urls": ["stun:stun.l.google.com:19302"]}]})
-        )
+        # Draw a label with a name below the face
+        cv2.rectangle(frame, (left, bottom - 35), (right, bottom), (0, 255, 0), cv2.FILLED)
+        font = cv2.FONT_HERSHEY_DUPLEX
+        cv2.putText(frame, name, (left + 4, bottom - 4), font, 1, (0,0,255), 2)
 
-if __name__ == "__main__":
-    app()
+    # Display the resulting image
+    cv2.imshow('Image', frame)
+
+    # Hit 'q' on the keyboard to quit!
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+# Release handle to the webcam
+capture.release()
+cv2.destroyAllWindows()
